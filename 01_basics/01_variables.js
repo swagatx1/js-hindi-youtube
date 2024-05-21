aac733950485acc86fca8e94e508faa4731385b7
Swagat Rout <swagatrout03@gmail.com>
2024-05-21 08:40:07 +0530

    02_dataTypes.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -19,4 +19,4 @@ because of issue in block scope and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([{"accountId":accountId, "accountEmail":accountEmail, "accountPassword":accountPassword, "accountCity":accountCity}])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert(3+3)  //we are using node js not brouser this can only run in brouser js
+
+console.log(3
+    +
+     3
+);  // code readeablity be high
+
+
+let name = "swagat"     //string => ""
+let age = 19            // int => fom -2^53 to +2^53 for bigger val use bigint
+let inLoggedIn = false  // boolean => 0/1
+let empty = null;       // null => standard representation of empty not 0
+let state;              // undined => 
+                        // symbol => unique
+
+//objects
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof state); // undefined
