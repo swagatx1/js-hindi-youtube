8ccb63c932f4c31bb052dcdadbfb9e424d161eb5
Swagat Rout <swagatrout03@gmail.com>
2024-05-24 06:59:49 +0530

    update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
-score = undefined
+score = "33"
 
 // const {score} = req.body
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score)); //same as above
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -26,5 +26,39 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//******************************************************************* */
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " swagat"
+// console.log(str1+str2);
+
+// console.log("1"+"2"); //12
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log(1+2); //3
+// console.log("1"+2+3); //123
+// console.log(1+"2"+3); //123
+// console.log(1+2+"3"); // 33
+
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // ERRROR
+// console.log(""); // 
+// console.log(+""); // 0
