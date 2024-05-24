07489fae955718b845730e6117acb8cf328eff43
Swagat Rout <swagatrout03@gmail.com>
2024-05-24 07:00:29 +0530

    04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 <= 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// avoid these type of comparisons
+// console.log(null >  0); // false
+// console.log(null <  0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// console.log(null <= 0); // true
+
+// avoid these type of comparisons too
+// console.log(undefined >  0); // false
+// console.log(undefined <  0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined >= 0); // false
+// console.log(undefined <= 0); // false
+
+// === STRICT CHECK this not only checks values but also there datatypes
+// console.log("2" == 2); // true
+// console.log("2" === 2); // false
+
