6a105b5c17807c5b0b158db429546595d75ea656
Swagat Rout <swagatrout03@gmail.com>
2024-06-25 05:36:16 +0530

    updated -> 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,35 @@
-const accountId = 144553
-let accountEmail = "swagat@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+// #1
+const accountId = 144553  // CONST  CANT BE CHANGED
+let accountEmail = "swagat@google.com" // LET
+var accountPassword = "12345" // VAR
+accountCity = "Jaipur" // DEFAULT
 
-// accountId = 2 // not allowed
+// accountId = 2 // TRYING TO REWRITE CONST VARIABLE ERROR
 
+// #2
+let accountState; // IF A VARIABLE IS NOT ASSIGNED VALUE IT HAS VALUE 'undefined' BY DEFAULT
+console.log("accountState : ", accountState)
 
+// #3
 accountEmail = "sw@hc.com"
 accountPassword = "21212121"
 accountCity = "pune"
 
-console.log(accountId);
+console.log("accountId : ",accountId);
+console.log("accountEmail : ",accountEmail);       // WRITING COLSOL.LOG IS REDUNDANT
+console.log("accountPassword : ",accountPassword); // INSTED YOU CAN USE CONSOLE.TABLE
+console.log("accountCity : ",accountCity);
+
+// CONSOLE.TABLE
+console.table([accountId, accountEmail,accountPassword,accountCity])
+console.table([{"accountId":accountId, "accountEmail":accountEmail, "accountPassword":accountPassword, "accountCity":accountCity}])
 
 /*
-Prefer not to use var
-because of issue in block scope and functional scope
+#4
+Use LET over VAR
+Reason - Because in earlier JS var variables would change global var when being used inside '{}' too 
+so it used to change the value of orignal var when for/while loop or any fun would run 
+that's why var in not used anymore
 */
 
 
-console.table([{"accountId":accountId, "accountEmail":accountEmail, "accountPassword":accountPassword, "accountCity":accountCity}])
