2249c33bd1ec4e2016aabad0c69c5ee474a747fc
Swagat Rout <swagatrout03@gmail.com>
2024-06-25 06:44:32 +0530

    update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,7 @@
 "use strict"; // treat all JS code as newer version
 
 
-// alert(3+3)  //we are using node js not brouser this can only run in brouser js
+// alert(3+3)  //we are using node js not browser this can only run in brouser js
 
 console.log(3
     +
@@ -14,7 +14,8 @@ let age = 19            // int => fom -2^53 to +2^53 for bigger val use bigint
 let inLoggedIn = false  // boolean => 0/1
 let empty = null;       // null => standard representation of empty not 0
 let state;              // undined => 
-                        // symbol => unique
+                        // bigint => to store bigger integer values
+                        // symbol => unique mainly used in react we will learn it in react 
 
 //objects
 
