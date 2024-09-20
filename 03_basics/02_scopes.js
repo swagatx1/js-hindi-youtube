869ec2c9e59f0c61c274e4cc2b2b9395b280f4e0
Swagat Rout <swagatrout03@gmail.com>
2024-09-21 01:44:29 +0530

    a

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,7 +4,7 @@ var c = 30
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 }
 // console.log(a);
@@ -16,11 +16,37 @@ function one(){
 
     function two(){
         const webite = "youtube"
-        console.log(uername);
+        console.log(username);
     }
-    console.log(website);
+    // console.log(website);
     
     two()
 }
 
 one()
+
+if(true){
+    const username = "swagat"
+    if(username == "swagat"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(webite);
+    
+}
+// console.log(username);
+
+
+// +++++++++++++++++ intresting +++++++++++++++++++++
+
+addone(5)
+function addone (num){
+    return num+1
+}
+
+
+// addTwo(5) // this will show an error 
+const addTwo = function(num){
+    return num + 2
+}
