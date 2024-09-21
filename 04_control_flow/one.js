cbc247e09d86d60a88cc879f20bc3842045a35cb
Swagat Rout <swagatrout03@gmail.com>
2024-09-22 03:53:44 +0530

    04_control_flow/one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if
+
+const isUserLoggedIn = true
+const temp = 41
+
+// if(temp === 40){
+//     console.log("less than 50");
+// }else{
+//     console.log("temp is graeter than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score  = 200
+// if(score > 100 ){
+//     const power = "fly"
+//     console.log(`User power${power}`);
+    
+// }
+
+// console.log(`User power${power}`); // ERROR
+
+
+const balance = 1000
+// if(balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than  900");
+// } else console.log("less than 1200");
+
+const userLoggedIn = true
+const debitCard = true
+const loggedItFromGoogle = false
+const loggedItFromEmail = false
+
+
+if (userLoggedIn && debitCard && 2==4){
+    console.log("Allowed to buy course");
+}
+
+if (loggedItFromGoogle || loggedItFromEmail){
+    console.log("User logged in");
+    
+}
+
+
+
+
+
