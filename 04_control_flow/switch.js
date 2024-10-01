69a7c05ca40b6b1f32bc242551ee57984a87b93e
Swagat Rout <swagatrout03@gmail.com>
2024-10-02 04:05:22 +0530

    switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default: console.log("NO Month Match");
+    
+}
