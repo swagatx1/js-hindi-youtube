e191c27a04336b785069eb6d493db310fea3e6a4
Swagat Rout <swagatrout03@gmail.com>
2024-10-02 17:51:56 +0530

    two.js

---
+++ 05_iterators/two.js
@@ -0,0 +1,23 @@
+
+let index = 1
+while(index<=10){
+    // console.log(`Value of indfex ${index}`);
+    index += 1
+}
+
+myArr = ["superman", "batman", "flash"]
+arr = 0
+while (arr < myArr.length) {
+    // console.log(`Character name is ${myArr[arr]}`);
+    arr+=1
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+} while (score <= 10);
+
+
+
